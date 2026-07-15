44de2b1f28a387175129087f729e00742b702371
roic71134-ship-it <roic71134@gmail.com>
2026-07-15 11:39:26 +0100

    Update main.py

--- frontend/main.py
+++ frontend/main.py
@@ -5,7 +5,7 @@ from views.auth.mfa import vue_mfa
 from views.auth.inscription import vue_inscription
 
 BREAKPOINT_MOBILE = 700
-IMAGE_FOND = "https://images.unsplash.com/photo-1544427920-c49ccfb85579?w=800&q=80"
+IMAGE_FOND = "F.jpg"
 
 
 def main(page: ft.Page):
