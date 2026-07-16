5858bd33d6a5d7ca85c453fe512fc559bbe4a22f
roic71134-ship-it <roic71134@gmail.com>
2026-07-16 14:21:58 +0100

    Update gestion.py

--- frontend/views/terrain/gestion.py
+++ frontend/views/terrain/gestion.py
@@ -3,8 +3,8 @@ from config import COULEURS
 
 
 def vue_gestion_terrain(page: ft.Page):
-    from api_client import get_client
-    client = get_client(page)
+    from api_client import client
+
 
     def snack(msg, couleur=None):
         page.snack_bar = ft.SnackBar(
